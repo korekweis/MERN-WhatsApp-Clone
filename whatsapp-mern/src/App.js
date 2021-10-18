129644247bf7600578503bc26940652ba6a745ae
korekweis <korekweis@gmail.com>
2021-10-17 23:58:51 -0700

    created react project

---
+++ whatsapp-mern/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1> Building a MERN WhatsApp Clone</h1>
+      {/* Sidebar */}
+      {/* Chat component */}
+    </div>
+  );
+}
+
+export default App;
