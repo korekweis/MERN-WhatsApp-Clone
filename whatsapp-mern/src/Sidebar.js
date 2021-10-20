3c47cd563a2a29e8bacab297b0b544bb09d25a1e
korekweis <korekweis@gmail.com>
2021-10-19 20:32:21 -0700

    added sidebar

--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import './Sidebar.css';
-import { Icon } from '@mui/material';
+import { IconButton, Avatar } from '@mui/material';
+import ChatIcon from '@mui/icons-material/Chat';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import SidebarChat from './SidebarChat';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1> I am a Sidebar </h1>
             <div className="sidebar_header">
-                <div className="sidebar_headerRight ">
-
+                <Avatar />
+                <div className="sidebar_headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
                 </div>
+            </div>
+
+            <div className="sidebar_search">
+                <div className="sidebar_searchContainer">
+                    <SearchOutlinedIcon />
+                    <input placeholder="Search or start now chat" type="text" />
+                </div>
+            </div>
+
+            <div className="sidebar_chats"> 
+                <SidebarChat />
             </div>
         </div>
     )
